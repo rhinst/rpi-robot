4beba3b5a5f897fbc2ae692ed779a1bc5b1b05a2
Rob Hinst <robert.hinst@crowncastle.com>
2020-12-27 18:25:18 -0500

    Adds motor driver code

--- app/subsystem/motor/driver.py
+++ app/subsystem/motor/driver.py
@@ -34,11 +34,32 @@ class MotorPins:
 
 class Driver(Subsystem):
     pins: MotorPins
+    fl_pwm: GPIO.PWM
+    fr_pwm: GPIO.PWM
+    rl_pwm: GPIO.PWM
+    rr_pwm: GPIO.PWM
 
     def __init__(self, message_bus: Connection, pins: MotorPins):
         super().__init__(message_bus)
         self.pins = pins
         GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(
+            [
+                pins.front_left_dir,
+                pins.front_left_speed,
+                pins.front_right_dir,
+                pins.front_right_speed,
+                pins.rear_left_dir,
+                pins.rear_left_speed,
+                pins.rear_right_dir,
+                pins.rear_right_speed,
+            ],
+            GPIO.OUT
+        )
+        self.fl_pwm = GPIO.PWM(pins.front_left_speed, 1000)
+        self.fr_pwm = GPIO.PWM(pins.front_right_speed, 1000)
+        self.rl_pwm = GPIO.PWM(pins.rear_left_speed, 1000)
+        self.rr_pwm = GPIO.PWM(pins.rear_right_speed, 1000)
 
     def register_commands(self):
         return [
@@ -60,12 +81,31 @@ class Driver(Subsystem):
         ]
 
     def on_drive_command(self, command: CommandMessage):
-        print("DRIVE THE MOTORS!")
+        motors = [
+            MotorState(
+                speed=abs(int(arg)),
+                direction=MotorDirection.FORWARD if int(arg) > 0 else MotorDirection.BACKWARD
+            )
+            for arg in command.arguments
+        ]
+        self.drive(*motors)
 
-    def on_stop_command(self, command: CommandMessage):
-        print("STOP THE MOTORS!")
+    def on_stop_command(self, _):
+        self.stop()
 
     def drive(self, front_left: MotorState, front_right: MotorState, rear_left: MotorState, rear_right: MotorState):
-        pass
+        self.fl_pwm.start(front_left.speed*100)
+        GPIO.output(self.pins.front_left_dir, GPIO.HIGH if front_left.direction == MotorDirection.FORWARD else GPIO.LOW)
+        self.fr_pwm.start(front_right.speed*100)
+        GPIO.output(self.pins.front_right_dir, GPIO.HIGH if front_right.direction == MotorDirection.FORWARD else GPIO.LOW)
+        self.rl_pwm.start(rear_left.speed*100)
+        GPIO.output(self.pins.rear_left_dir, GPIO.HIGH if rear_left.direction == MotorDirection.FORWARD else GPIO.LOW)
+        self.rr_pwm.start(rear_right.speed*100)
+        GPIO.output(self.pins.rear_right_dir, GPIO.HIGH if rear_right.direction == MotorDirection.FORWARD else GPIO.LOW)
 
+    def stop(self):
+        self.fl_pwm.stop()
+        self.fr_pwm.stop()
+        self.rl_pwm.stop()
+        self.rr_pwm.stop()
 
