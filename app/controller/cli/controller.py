16473c6f5cbce2b2fa539f0425ff4baace9900c2
Rob Hinst <robert.hinst@crowncastle.com>
2020-12-30 10:47:05 -0500

    Refactored CLI controller to be more easily testable

--- app/controller/cli/controller.py
+++ app/controller/cli/controller.py
@@ -1,3 +1,5 @@
+from itertools import cycle
+
 from app.message_bus import Connection, CommandMessage
 from app.logging import logger
 
@@ -10,18 +12,19 @@ class CliController:
     def __init__(self, message_bus: Connection):
         self.message_bus = message_bus
 
+    def process_input(self, user_input: str):
+        # TODO: more sophisticated command parsing (strings can be quoted with spaces in them)
+        command_parts = user_input.split()
+        command = command_parts[0]
+        arguments = command_parts[1:]
+        logger.debug("Received command from CLI: '%s'", command)
+        self.message_bus.publish(
+            CommandMessage(channel="core.command", command=command, arguments=arguments)
+        )
+
     def start(self):
-        while True:
+        while cycle([True]):
             user_input = input(PROMPT).strip()
             if len(user_input) == 0:
                 continue
-            # TODO: more sophisticated command parsing (strings can be quoted with spaces in them)
-            command_parts = user_input.split()
-            command = command_parts[0]
-            arguments = command_parts[1:]
-            logger.debug("Received command from CLI: '%s'", command)
-            self.message_bus.publish(
-                CommandMessage(
-                    channel="core.command", command=command, arguments=arguments
-                )
-            )
+            self.process_input(user_input)
